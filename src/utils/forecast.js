de90fe867809f5178f8b84ad2ab89c21b2598e30
PuneetAsnani12 <asnani.puneet@gmail.com>
2019-12-25 21:24:36 +0530

    add new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request= require('request')
+
 const forecast = (latitude,longitude ,callback)=>{
     const url= 'https://api.darksky.net/forecast/3201269345429a0b48b191b6c3b0af06/'+latitude+','+longitude
     request({url,json:true},(error,{body})=>{
@@ -9,7 +10,7 @@ const forecast = (latitude,longitude ,callback)=>{
             callback('Unable to find location',undefined)
         }
         else{
-            callback(undefined,body.daily.data[0].summary+' It is curently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain.')
+            callback(undefined,body.daily.data[0].summary+' It is curently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain. Low and High temperatures being '+body.daily.data[0].temperatureLow +' degrees and '+body.daily.data[0].temperatureHigh +' degrees respectively.')
         }
     })
 }
